e0f50e49fc6830cd6d8890baafbcc5cb623f812c
gusfrancoo <gustafranco2543@gmail.com>
2024-10-28 15:38:27 -0300

    first commit

---
+++ src/test/java/br/com/aula/faculdade/FaculdadeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aula.faculdade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaculdadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
